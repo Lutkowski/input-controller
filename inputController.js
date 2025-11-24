4ea641cc02dd9f56de974e36544a24896abb4539
Lutkowski <anton.lutkovskiy@gmail.com>
2025-11-24 12:29:58 +0300

    feat: bindActions method

--- inputController.js
+++ inputController.js
@@ -1,1 +1,16 @@
-console.log(2)
+export class InputContoller {
+    constructor(actionsToBind = {}, target) {
+        this.enabled = false;
+        this.focus = true;
+        this.target = target;
+        this.actions = {};
+        this.bindActions(actionsToBind);
+    }
+
+    bindActions(actionsToBind) {
+        for (const action in actionsToBind) {
+            const actionConfig = actionsToBind[action];
+            this.actions[action] = actionConfig;
+        }
+    }
+}
